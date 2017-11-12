db2c069d36dab1931445a8a79613eafc29efdaf2
Rebecca Howard <rebeccalhoward@gmail.com>
2017-11-12 20:50:36 +0000

    Add total count

--- server/src/basicFunction.js
+++ server/src/basicFunction.js
@@ -1,70 +1,23 @@
-// export default async event => {
-//     // you can use ES7 with async/await and even TypeScript in your functions :)
+import { fromEvent } from 'graphcool-lib'
   
-//     await new Promise(r => setTimeout(r, 50))
-  
-//     return {
-//       data: {
-//         totalPosts: `Hello ${event.data.name || 'World'}`
-//       }
-//     }
-//   }
-
-  // const fromEvent = require('graphcool-lib').fromEvent
-  
-  
-  // module.exports = function(event) {
-  //   const graphcool = fromEvent(event)
-  //   const api = graphcool.api('simple/v1')
-
-  //   const queryResult = api.request(`
-  //     query {
-  //       _allPostsMeta{
-  //         totalPosts
-  //       }
-  //     }`)
-
-  //   return {
-  //     data: {
-  //       totalPosts: `Hello ${event.data.name || 'World'}`
-  //     }
-  //   }
-  // }
-  
-  // import { fromEvent } from 'graphcool-lib'
-  
-  // export default async event => {
-  //   const lib = fromEvent(event)
-  //   const client = lib.api('simple/v1')
-  //   const allUsers = await client.request(`
-  //     query {
-  //       allUsers {
-  //           name
-  //         }
-  //       }
-  //   `).then((response) => {
-  //     console.log(response)
-  //     return {
-  //       data: {
-  //         totalPosts: "allUsers._allPostsMeta.totalPosts"
-  //       }
-  //     }
-  //   })
-  // }
-
-  import { fromEvent } from 'graphcool-lib'
-  
-  export default async event => {
-    const lib = fromEvent(event)
-    const client = lib.api('simple/v1')
-    const res = await client.request(`
-      Page(id: "cj9tp9ahgfxe40112nvdpec68") {
-        title
-      }`)
-  
-    return {
-      data: {
-        totalPosts: res.Page.title
-      }
+export default async event => {
+  const userID = event.context.auth.nodeId;
+  const lib = fromEvent(event);
+  const client = lib.api('simple/v1');
+  const { allPosts, _allPostsMeta } = await client.request(`{
+    allPosts { 
+      id
     }
-  }
+    _allPostsMeta (filter: {
+      id: "${userID}"
+    }) { 
+      count 
+    }
+  }`);
+
+  return {
+    data: {
+      totalPosts: `You have posted ${userID} ${_allPostsMeta.count} of ${allPosts.length} posts!`
+    }
+  };
+}
